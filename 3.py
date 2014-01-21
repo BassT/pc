56d69fad878aaa391b64cb6b39e9a4a20f662527
Sebastian Richter <sebastian.richter@student.kit.edu>
2014-01-21 17:40:53 -0500

    Challenge 3 - solved

--- 3.py
+++ 3.py
@@ -3,12 +3,14 @@
 import sys
 import re
 foundLetters = []
-pattern = re.compile('[A-Z]{3}[a-z]{1}[A-Z]{3}')
+pattern = re.compile('[^A-Z]+[A-Z]{3}[a-z]{1}[A-Z]{3}[^A-Z]+')
+pattern2 = re.compile('[A-Z]{3}[a-z]{1}[A-Z]{3}')
 with open('commentFromEqualityHtml.txt') as f:
         for line in f:
         	m = pattern.search(line)
         	if m != None:
         		sys.stdout.write('Found: ' + m.group() + '\n')
+        		m = pattern2.search(m.group())
         		foundLetters.append(m.group()[3])
 sys.stdout.write(''.join(foundLetters))
 sys.stdout.flush()
