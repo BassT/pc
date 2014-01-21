89eeaf77186de5c370d32b7489ef2c1df5a7b91c
Sebastian Richter <sebastian.richter@student.kit.edu>
2014-01-21 17:51:38 -0500

    Challenge 4 - notes

---
+++ 4.py
@@ -0,0 +1,6 @@
+##follow the chain
+##chain: an url ...?nothing=SomeNumber contains one line 'and the next nothing is NextNumber'
+##Hint: DON'T TRY ALL NOTHINGS, since it will never 
+##end. 400 times is more than enough. 
+##
+##Request urls one after the other, if there's something else written than the phrase --> halt, output
