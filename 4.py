c4e9628a7d3d73f7ad573390ca2efd629dbd426b
Sebastian Richter <sebastian.richter@student.kit.edu>
2014-01-25 16:37:54 -0500

    Challenge 5 - solved

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-5##follow the chain
+##follow the chain
 ##chain: an url ...?nothing=SomeNumber contains one line 'and the next nothing is NextNumber'
 ##Hint: DON'T TRY ALL NOTHINGS, since it will never 
 ##end. 400 times is more than enough. 
--- 5.py
+++ 5.py
@@ -1,2 +1,14 @@
 ## found banner.p file in source view
 ## google yields: .p is python pickle file - something with serialization
+
+import pickle, pprint
+
+with open("banner.p", "rb") as f:
+	data = pickle.load(f)
+##	pprint.pprint(data)
+
+for nested_list in data:
+	string = ""
+	for element in nested_list:
+		string = string + (element[0] * element[1])
+	print(string)
